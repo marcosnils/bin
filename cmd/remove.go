7e0952cf52252fc72bc1fbbb11b16bcaa390ab96
Marcos Lilljedahl <marcosnils@gmail.com>
2020-06-24 10:12:08 -0300

    Add remove

--- cmd/remove.go
+++ cmd/remove.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/apex/log"
+	"github.com/marcosnils/bin/pkg/config"
 	"github.com/spf13/cobra"
 )
 
@@ -16,12 +21,34 @@ func newRemoveCmd() *removeCmd {
 	var root = &removeCmd{}
 	// nolint: dupl
 	var cmd = &cobra.Command{
-		Use:           "remove",
-		Aliases:       []string{"r"},
+		Use:           "remove <paths...>",
+		Aliases:       []string{"rm"},
 		Short:         "Removes binaries managed by bin",
 		SilenceUsage:  true,
+		Args:          cobra.MinimumNArgs(1),
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
+
+			cfg := config.Get()
+
+			existingToRemove := []string{}
+
+			for _, p := range args {
+				if _, ok := cfg.Bins[p]; ok {
+					existingToRemove = append(existingToRemove, p)
+					//TODO some providers (like docker) might download
+					//additional things somewhere else, maybe we should
+					//call the provider to do a cleanup here.
+					err := os.Remove(p)
+					if err != nil {
+						return fmt.Errorf("Error removing path %s: %v", p, err)
+					}
+					continue
+				}
+				log.Infof("Path %s not found in bin, ignoring.", p)
+			}
+
+			config.RemoveBinaries(existingToRemove)
 			return nil
 		},
 	}
