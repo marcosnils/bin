1e4d59085ecfa8dd0558d237407382eda76c5d3c
Marcos Lilljedahl <marcosnils@gmail.com>
2020-08-09 12:41:01 -0300

    Fix missing type change

--- pkg/config/config_windows.go
+++ pkg/config/config_windows.go
@@ -14,7 +14,7 @@ import (
 //user in the system
 //TODO add feature to prompt the user which to select
 //if many paths are found
-func getDefaultPath() string {
+func getDefaultPath() (string, error) {
 	penv := os.Getenv("PATH")
 	log.Debugf("User PATH is [%s]", penv)
 	opts := []fmt.Stringer{}
@@ -35,6 +35,10 @@ func getDefaultPath() string {
 
 	}
 
-	return options.Select("Pick a default download dir: ", opts).(options.LiteralStringer).String()
+	choice, err := options.Select("Pick a default download dir: ", opts)
+	if err != nil {
+		return "", err
+	}
+	return choice.(fmt.Stringer).String(), nil
 
 }
