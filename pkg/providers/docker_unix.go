1abfb2abd2f20d9a724afc9f3d20814f31b2b16e
Marcos Nils <marcosnils@gmail.com>
2020-11-22 10:33:21 -0300

    Check if stdin is a tty and add docker flag accordingly (#34)
    Fixes #33
    
    TODO: do the same thing in powershell for the windows provider

--- pkg/providers/docker_unix.go
+++ pkg/providers/docker_unix.go
@@ -9,7 +9,9 @@ const (
 	// TODO: there might be a way were users can configure a template for the
 	// actual execution since some CLIs require some other folders to be mounted
 	// or networks to be shared
-	sh = `docker run --rm -i -t -v ${PWD}:/tmp/cmd -w /tmp/cmd %s:%s "$@"`
+	sh = `
+	termflag=$([ -t 0 ] && echo -n "-t")
+	docker run --rm -i $termflag -v ${PWD}:/tmp/cmd -w /tmp/cmd %s:%s "$@"`
 )
 
 // getImageName gets the name of the image from the image repo.
