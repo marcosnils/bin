0b639e13f490f4d7d07e7a1d4a31f6bb47388061
Roberto Ferrer <r.ferrer@innovation.es>
2021-03-03 22:04:56 +0100

    initialize sample scenarios based on OS

--- pkg/assets/assets_test.go
+++ pkg/assets/assets_test.go
@@ -1,20 +1,30 @@
 package assets
 
 import (
+	"runtime"
 	"testing"
 )
 
 func TestSanitizeName(t *testing.T) {
-	cases := []struct {
+	type sample struct {
 		in  string
 		v   string
 		out string
-	}{
-		{"bin_amd64_linux", "v0.0.1", "bin"},
-		{"bin_0.0.1_amd64_linux", "0.0.1", "bin"},
-		{"bin_0.0.1_amd64_linux", "v0.0.1", "bin"},
-		{"gitlab-runner-linux-amd64", "v13.2.1", "gitlab-runner"},
-		{"jq-linux64", "jq-1.5", "jq"},
+	}
+	var cases []sample
+	if runtime.GOOS == "windows" {
+		cases = []sample{
+			{"bin_0.0.1_Windows_x86_64.exe","0.0.1","bin.exe"},
+		}
+	} else {
+		cases = []sample{
+			{"bin_amd64_linux", "v0.0.1", "bin"},
+			{"bin_0.0.1_amd64_linux", "0.0.1", "bin"},
+			{"bin_0.0.1_amd64_linux", "v0.0.1", "bin"},
+			{"gitlab-runner-linux-amd64", "v13.2.1", "gitlab-runner"},
+			{"jq-linux64", "jq-1.5", "jq"},
+
+		}
 	}
 
 	for _, c := range cases {
@@ -30,40 +40,52 @@ func TestFilterAssets(t *testing.T) {
 		repoName string
 		as       []*Asset
 	}
-	cases := []struct {
+	type sample struct {
 		in  args
 		out string
-	}{
-		{args{"bin", []*Asset{
-			{Name: "bin_0.0.1_Linux_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.0.1_Linux_x86_64"},
-			{Name: "bin_0.0.1_Linux_i386", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.0.1_Linux_i386"},
-			{Name: "bin_0.0.1_Darwin_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.0.1_Darwin_x86_64"},
-		}}, "bin_0.0.1_linux_x86_64"},
-		{args{"bin", []*Asset{
-			{Name: "bin_0.1.0_Windows_i386.exe", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Windows_i386.exe"},
-			{Name: "bin_0.1.0_Linux_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Linux_x86_64"},
-			{Name: "bin_0.1.0_Darwin_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Darwin_x86_64"},
-		}}, "bin_0.1.0_linux_x86_64"},
-		{args{"bin", []*Asset{
-			{Name: "bin_0.1.0_Windows_i386.exe", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Windows_i386.exe"},
-			{Name: "bin_0.1.0_Linux_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Linux_x86_64"},
-			{Name: "bin_0.1.0_Darwin_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Darwin_x86_64"},
-		}}, "bin_0.1.0_linux_x86_64"},
-		{args{"gitlab-runner", []*Asset{
-			{Name: "Windows 64 bits", URL: "https://gitlab-runner-downloads.s3.amazonaws.com/v13.2.1/binaries/gitlab-runner-windows-amd64.zip"},
-			{Name: "linux amd64", URL: "https://gitlab-runner-downloads.s3.amazonaws.com/v13.2.1/binaries/gitlab-runner-linux-amd64"},
-			{Name: "macOS", URL: "https://gitlab-runner-downloads.s3.amazonaws.com/v13.2.1/binaries/gitlab-runner-darwin-amd64"},
-		}}, "gitlab-runner-linux-amd64"},
-		{args{"yq", []*Asset{
-			{Name: "yq_freebsd_amd64", URL: "https://github.com/mikefarah/yq/releases/download/3.3.2/yq_freebsd_amd64"},
-			{Name: "yq_linux_amd64", URL: "https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64"},
-			{Name: "yq_windows_amd64.exe", URL: "https://github.com/mikefarah/yq/releases/download/3.3.2/yq_windows_amd64.exe"},
-		}}, "yq_linux_amd64"},
-		{args{"jq", []*Asset{
-			{Name: "jq-win64.exe", URL: "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-win64.exe"},
-			{Name: "jq-linux64", URL: "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64"},
-			{Name: "jq-osx-amd64", URL: "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-osx-amd64"},
-		}}, "jq-linux64"},
+	}
+	var cases []sample
+	if runtime.GOOS == "windows"{
+		cases = []sample {
+			{args{"bin", []*Asset{
+				{Name: "bin_0.0.1_Windows_x86_64.exe", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.0.1_Windows_x86_64.exe"},
+				{Name: "bin_0.1.0_Linux_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Linux_x86_64"},
+				{Name: "bin_0.1.0_Darwin_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Darwin_x86_64"},
+			}}, "bin_0.0.1_windows_x86_64.exe"},
+		}
+	} else {
+		cases = []sample{
+			{args{"bin", []*Asset{
+				{Name: "bin_0.0.1_Linux_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.0.1_Linux_x86_64"},
+				{Name: "bin_0.0.1_Linux_i386", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.0.1_Linux_i386"},
+				{Name: "bin_0.0.1_Darwin_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.0.1_Darwin_x86_64"},
+			}}, "bin_0.0.1_linux_x86_64"},
+			{args{"bin", []*Asset{
+				{Name: "bin_0.1.0_Windows_i386.exe", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Windows_i386.exe"},
+				{Name: "bin_0.1.0_Linux_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Linux_x86_64"},
+				{Name: "bin_0.1.0_Darwin_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Darwin_x86_64"},
+			}}, "bin_0.1.0_linux_x86_64"},
+			{args{"bin", []*Asset{
+				{Name: "bin_0.1.0_Windows_i386.exe", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Windows_i386.exe"},
+				{Name: "bin_0.1.0_Linux_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Linux_x86_64"},
+				{Name: "bin_0.1.0_Darwin_x86_64", URL: "https://github.com/marcosnils/bin/releases/download/v0.0.1/bin_0.1.0_Darwin_x86_64"},
+			}}, "bin_0.1.0_linux_x86_64"},
+			{args{"gitlab-runner", []*Asset{
+				{Name: "Windows 64 bits", URL: "https://gitlab-runner-downloads.s3.amazonaws.com/v13.2.1/binaries/gitlab-runner-windows-amd64.zip"},
+				{Name: "linux amd64", URL: "https://gitlab-runner-downloads.s3.amazonaws.com/v13.2.1/binaries/gitlab-runner-linux-amd64"},
+				{Name: "macOS", URL: "https://gitlab-runner-downloads.s3.amazonaws.com/v13.2.1/binaries/gitlab-runner-darwin-amd64"},
+			}}, "gitlab-runner-linux-amd64"},
+			{args{"yq", []*Asset{
+				{Name: "yq_freebsd_amd64", URL: "https://github.com/mikefarah/yq/releases/download/3.3.2/yq_freebsd_amd64"},
+				{Name: "yq_linux_amd64", URL: "https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64"},
+				{Name: "yq_windows_amd64.exe", URL: "https://github.com/mikefarah/yq/releases/download/3.3.2/yq_windows_amd64.exe"},
+			}}, "yq_linux_amd64"},
+			{args{"jq", []*Asset{
+				{Name: "jq-win64.exe", URL: "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-win64.exe"},
+				{Name: "jq-linux64", URL: "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64"},
+				{Name: "jq-osx-amd64", URL: "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-osx-amd64"},
+			}}, "jq-linux64"},
+		}
 	}
 
 	for _, c := range cases {
