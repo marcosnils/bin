83339d48cfe247c8ea5491f5e3e9bbdb8f952292
Marcos Nils <marcosnils@gmail.com>
2021-04-01 01:03:44 -0300

    Update cmd/update.go
    Co-authored-by: Lucas Bremgartner <breml@users.noreply.github.com>

--- cmd/update.go
+++ cmd/update.go
@@ -78,8 +78,10 @@ func newUpdateCmd() *updateCmd {
 			if len(toUpdate) == 0 {
 				log.Infof("All binaries are up to date")
 				return nil
-			} else if root.opts.dryRun {
-				return fmt.Errorf("Command aborted, dry-run mode")
+			}
+			
+			if root.opts.dryRun {
+				return fmt.Errorf("Updates found, exit (dry-run mode).")
 			}
 
 			// TODO will have to refactor this prompt to a separate function
